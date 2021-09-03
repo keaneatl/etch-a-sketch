0128addfc902d9373c4b31179f6d35d33d36b8fb
usrwatermelon <kyntltntn@protonmail.com>
2021-09-03 14:09:35 +0000

    fixed a bug where the current grid disappears when pressing the new grid button, and when pressing the cancel button afterwards

--- scripts/main.js
+++ scripts/main.js
@@ -49,15 +49,17 @@ clearBtn.addEventListener('click', () => {
 
 // Generate New Grid
 newGridBtn.addEventListener('click', () => {
-    let existingBoxes = document.querySelectorAll('.boxes');
-    let existingArrs = Array.from(existingBoxes);
-    existingArrs.forEach((item) =>{
-        item.remove();
-    });
-    container.removeAttribute('style');
-    let gridNum = prompt("Please enter the number of squares per side (16 will generate a 16x16 grid)");
-    container.setAttribute('style', `display: grid; grid-template-columns: repeat(${gridNum}, auto); border: 1px solid black; position: relative; width: 400px; height: 400px`);
+
+    let gridNum = prompt("Please enter the number of squares per side (16 will generate a 16x16 grid)", 16);
         if (gridNum <= 100 && gridNum > 0){
+            let existingBoxes = document.querySelectorAll('.boxes');
+            let existingArrs = Array.from(existingBoxes);
+            existingArrs.forEach((item) =>{
+                item.remove();
+            });
+            container.removeAttribute('style');
+
+            container.setAttribute('style', `display: grid; grid-template-columns: repeat(${gridNum}, auto); border: 1px solid black; position: relative; width: 400px; height: 400px`);
             for (let i = 0; i < gridNum*gridNum; i++){
             newGrid = document.createElement('div');
             newGrid.setAttribute('style', 'grid-auto-flow: column;');
